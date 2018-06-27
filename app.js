5924ebfdc1448670c64bee36fde01aafb1730796
Marco Mercado <mmerc007@ucr.edu>
2018-06-26 20:32:41 -0700

    back end of the first page

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express();
 app.set("view engine", "ejs");
 
-app.get("/", function(){
-    console.log("testing main");
+app.get("/", function(req,res){
+    res.render("main");
 
 });
 
-app.listen(3000,function(){
+app.listen(3000,function(req,res){
 
     console.log("Server has started");
 });
