d5c8e0e3ca38170b233e647298be71e8d284d289
Marco Mercado <mmerc007@ucr.edu>
2018-07-14 23:20:33 -0700

    initial mongoose database set up

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express = require('express');
-
+var mongoose = require("mongoose")
 var app = express();
+
+mongoose.connect("mongodb://localhost/Gallery");
 app.set("view engine", "ejs");
+// the schema of the ballery
+var GallerySchema = new mongoose.Schema({
+  Photo: String,
+  Caption: String,
+  created: {type: Date, default: Date.now}
+});
+var Gallery = mongoose.model("Gallery", GallerySchema);
+
+Gallery.create({
+  Photo: String,
+  Caption: String,
+  created: {type: Date, default: Date.now}
+
+
+});
+
 
 app.get("/", function(req,res){
     res.render("main");
